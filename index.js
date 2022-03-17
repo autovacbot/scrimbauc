ef468eebd7e1af8799c75e6de7fd08e7aefefd37
autovacbot <autovacbot@gmail.com>
2022-03-18 00:10:19 +0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-JSON
+let length = document.getElementById("length-result")
+let volume = document.getElementById("volume-result")
+let mass = document.getElementById("mass-result")
+
+console.log(length)
