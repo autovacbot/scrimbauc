3608ba0ab08443dcf06276f8f58c28252e62d522
autovacbot <autovacbot@gmail.com>
2022-03-18 10:32:12 +0800

    ftgyh

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ let length = document.getElementById("length-result")
 let volume = document.getElementById("volume-result")
 let mass = document.getElementById("mass-result")
 
-console.log(length)
+console.log(length)zzz
